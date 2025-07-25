1b98cfdc1851cdf57d6d319ab986636e899886b0
sumukhacharya03 <sumukh.acharya@gmail.com>
2025-07-26 01:44:54 +0530

    Updated producer1.py code to send messages in JSON format

--- baseline_market_value/producer1.py
+++ baseline_market_value/producer1.py
@@ -1,10 +1,55 @@
+import csv
+import json
 from kafka import KafkaProducer
 
-topic_name='drivers_baseline_value'
-producer=KafkaProducer(bootstrap_servers='localhost:9092')
+topic_name = 'drivers-baseline-value'
 
-with open('drivers_baseline_value.csv', 'r') as file:
-    for line in file:
-        producer.send(topic_name, value=line.encode('utf-8'))
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    # Serializes dictionary to JSON bytes
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+with open('drivers_baseline_value.csv', mode='r', encoding='utf-8') as file:
+    csv_reader = csv.DictReader(file) # Reads rows as dictionaries
+    for row in csv_reader:
+        # Converts the values to numbers like 78M to 78000000
+        try:
+            value_str = row['Baseline Value'].strip().replace('$', '').replace(' ', '')
+            if 'M' in value_str:
+                value_num = float(value_str.replace('M', '')) * 1_000_000
+            else: # To add more conditions later if needed for cases other than M
+                value_num = float(value_str)
+
+            message = {
+                'driver_name': row['Driver Name'],
+                'baseline_value': int(value_num)
+            }
+            
+            # I am using the driver's name as the key
+            producer.send(topic_name, key=row['Driver Name'].encode('utf-8'), value=message)
+            print(f"Message Sent: {message}")
+            
+        except (ValueError, AttributeError) as e:
+            print(f"Skipping row due to parsing error: {row} - {e}")
+
 
 producer.flush()
+print("All messages sent successfully")
+
+
+# Kafka Commands for later (all in Kafka Directory)
+# 1. Create Topic:
+# ./kafka-topics.sh --create --topic drivers-baseline-value --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+# 2. Delete Topic:
+# ./kafka-topics.sh --delete --topic drivers-baseline-value --bootstrap-server localhost:9092
+# 3. Check list of Topics:
+# ./kafka-topics.sh --list --bootstrap-server localhost:9092
+# 4. To go to Kafka Directory:
+# cd ~/kafka_2.13-3.7.0/bin
+# 5. Check messages sent to a Topic:
+# ./kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic drivers-baseline-value --from-beginning
+# 6. Keep Zookeeper running:
+# bin/zookeeper-server-start.sh config/zookeeper.properties
+# 7. Keep Kafka Broker running (in another terminal):
+# bin/kafka-server-start.sh config/server.properties
